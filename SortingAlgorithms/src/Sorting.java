056dbb78c95d02c65dc40943a4ff02996590cd8c
huynh268 <huynhletien@gmail.com>
2017-10-14 23:13:05 -0500

    Added Merge sort

--- SortingAlgorithms/src/Sorting.java
+++ SortingAlgorithms/src/Sorting.java
@@ -14,7 +14,6 @@ public class Sorting {
         for(int i = 0; i < n; i++) {
             array[i] = random.nextInt(n);
         }
-
         return array;
     }
 
@@ -31,7 +30,7 @@ public class Sorting {
     //Bubble sort
     public int[] bubbleSort(int[] a) {
         int len = a.length;
-        int[] result = copyArray(a);
+        int[] result = copyArray(a, 0, len);
         int temp;
 
         for(int i = 0; i < len-1; i++) {
@@ -43,14 +42,13 @@ public class Sorting {
                 }
             }
         }
-
         return result;
     }
 
     //Bubble sort optimized
     public int[] bubbleSortOptimized(int[] a) {
         int len = a.length;
-        int[] result = copyArray(a);
+        int[] result = copyArray(a, 0, len);
         boolean swapped =  true;
         int temp;
         int i = 0;
@@ -65,34 +63,64 @@ public class Sorting {
                 }
             }
         }
+        return result;
+    }
 
+    //Merge sort
+    public int[] mergeSort(int[] a) {
+        int len = a.length;
+        if(len <= 1) return a;
+        int middle = len/2;
+        int[] left = copyArray(a, 0, middle);
+        int[] right = copyArray(a, middle, len);
+
+        return merge(mergeSort(left),mergeSort(right));
+    }
+
+
+    //Merge 2 arrays
+    private int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length+right.length];
+        int l = 0, r = 0;
+
+        for(int i = 0; i < result.length; i++) {
+            if(r >= right.length) result[i] = left[l++];
+            else if(l >= left.length) result[i] = right[r++];
+            else if(left[l] > right[r]) result[i] = right[r++];
+            else result[i] = left[l++];
+        }
         return result;
     }
 
     //Copy array
-    private int[] copyArray(int[] a){
-        int[] copy = new int[a.length];
-        System.arraycopy(a,0,copy,0,a.length);
+    private int[] copyArray(int[] a, int start, int end){
+        int[] copy = new int[end - start];
+        int length = end - start;
+        System.arraycopy(a,start,copy,0,length);
         return copy;
     }
 
     public static void main(String[] args) {
         Sorting sorting = new Sorting();
         int[] array =  sorting.generateRandomArray();
-        long startTime = System.nanoTime();
-        int[] result = sorting.bubbleSortOptimized(array);
-        long endTime = System.nanoTime();
-        System.out.println("Running time(nano): " + (endTime-startTime));
+
         System.out.print("Random Integers: ");
         for(int i: array) {
             System.out.print(i+" ");
         }
         System.out.println();
-        System.out.print("After sorting: ");
-        for(int i: result) {
-            System.out.print(i+" ");
-        }
-        System.out.println();
-        System.out.println("isSorted: " + sorting.isSorted(result));
+
+        long startTime = System.nanoTime();
+        int[] result = sorting.bubbleSortOptimized(array);
+        long endTime = System.nanoTime();
+        System.out.println("isSorted (Bubble sort): " + sorting.isSorted(result));
+
+        long startTime2 = System.nanoTime();
+        int[] result2 = sorting.mergeSort(array);
+        long endTime2 = System.nanoTime();
+        System.out.println("isSorted (Merge sort) : " + sorting.isSorted(result2));
+
+        System.out.println("Runtime Bubble sort: " + (endTime-startTime));
+        System.out.println("Runtime Merge sort : " + (endTime2-startTime2));
     }
 }
