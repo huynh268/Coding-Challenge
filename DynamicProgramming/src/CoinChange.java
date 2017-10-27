7482f8f60a168238e4d02c55379be7e9621dc1b3
huynh268 <huynhletien@gmail.com>
2017-10-26 21:06:35 -0500

    Added CoinChange

---
+++ DynamicProgramming/src/CoinChange.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Tien on 10/26/2017.
+ */
+public class CoinChange {
+
+    public static int coinChangeNumberOfWays(int[] coins, int total) {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
