2b9fcc361f1cdaddc2ee3bb052a077922692c063
huynh268 <huynhletien@gmail.com>
2017-10-28 23:49:46 -0500

    Added CoinChange

--- DynamicProgramming/src/CoinChange.java
+++ DynamicProgramming/src/CoinChange.java
@@ -4,10 +4,25 @@
 public class CoinChange {
 
     public static int coinChangeNumberOfWays(int[] coins, int total) {
-        return 1;
+        int[][] dp = new int[coins.length+1][total+1];
+
+        for(int i = 0; i < coins.length+1; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int i = 1; i < coins.length+1; i++) {
+            for(int j = 1; j < total+1; j++) {
+                if(coins[i-1] > j) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                }
+            }
+        }
+        return dp[coins.length][total];
     }
 
     public static void main(String[] args) {
-
+        System.out.println("coins [3,4,5,6,7,9], total 15: "+ coinChangeNumberOfWays(new int[]{3,4,5,6,7,9}, 15));
     }
 }
