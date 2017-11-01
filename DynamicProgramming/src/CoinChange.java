630e11f18ab38ebd7dcc0d882b0674e2c04b7767
huynh268 <huynhletien@gmail.com>
2017-10-31 21:29:08 -0500

    Added CoinChange method 2
    Use Dynamic Programming with 1D array

--- DynamicProgramming/src/CoinChange.java
+++ DynamicProgramming/src/CoinChange.java
@@ -37,10 +37,37 @@ public class CoinChange {
         return dp[coins.length][total];
     }
 
+    public static int coinChangeNumberOfWays2(int[] coins, int total) {
+        int[] dp = new int[total+1];
+        dp[0] = 1;
+
+        for (int i = 0; i < coins.length; i++) {
+            for(int j = 1; j < total+1; j++) {
+                if(coins[i] <= j) {
+                    dp[j] += dp[j-coins[i]];
+                }
+            }
+        }
+
+        
+        for (int j = 0; j < total+1; j++) {
+
+            System.out.printf("%4d", j);
+        }
+        System.out.println();
+
+        for (int i = 0; i < total+1; i++) {
+            System.out.printf("%4d", dp[i]);
+        }
+        System.out.println();
+
+        return dp[total];
+    }
 
 
     public static void main(String[] args) {
         System.out.println("coins [3,4,5,6,7,9], total 15: "+ coinChangeNumberOfWays(new int[]{3,4,5,6,7,9}, 15));
         System.out.println();
+        System.out.println("coins [3,4,5,6,7,9], total 15: "+ coinChangeNumberOfWays2(new int[]{3,4,5,6,7,9}, 15));
     }
 }
