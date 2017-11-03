5f6c93a478a5d4e4e950a3b036d1e3942321ea6f
huynh268 <huynhletien@gmail.com>
2017-11-02 19:29:27 -0500

    Added ValidParentheses

---
+++ Leetcode/src/ValidParentheses20.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+/**
+ * Created by Tien on 11/2/2017.
+ */
+public class ValidParentheses20 {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("String \"[\" isValid? " + isValid("["));
+        System.out.println("String \"()[]{}\" isValid? " + isValid("()[]{}"));
+        System.out.println("String \"(([({()})]))((())){[{}]}\" isValid? " + isValid("(([({()})]))((())){[{}]}"));
+    }
+}
