231319e0def8f19253f3de41b916698001965ea0
huynh268 <huynhletien@gmail.com>
2017-11-05 22:06:41 -0600

    Update RotateImage48

--- Leetcode/src/RotateImage48.java
+++ Leetcode/src/RotateImage48.java
@@ -1,34 +1,31 @@
 public class RotateImage48 {
     /**
     Given input matrix = 
-    [
-        [1,2,3],
+    [   [1,2,3],
         [4,5,6],
-        [7,8,9]
-    ],
-
+        [7,8,9]     ],
     rotate the input matrix in-place such that it becomes:
-    [
-        [7,4,1],
+    [   [7,4,1],
         [8,5,2],
-        [9,6,3]
-    ]
+        [9,6,3]     ]
     */
     public static void rotate(int[][] matrix) {
-        for(int i = 0; i<matrix.length; i++){
-            for(int j = i; j<matrix[0].length; j++){
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for(int i = 0; i < row; i++){
+            for(int j = i; j < col; j++){
                 int temp = 0;
                 temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-        for(int i =0 ; i<matrix.length; i++){
-            for(int j = 0; j<matrix.length/2; j++){
+        for(int i =0 ; i < row; i++){
+            for(int j = 0; j < row/2; j++){
                 int temp = 0;
                 temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length-1-j];
-                matrix[i][matrix.length-1-j] = temp;
+                matrix[i][j] = matrix[i][row-1-j];
+                matrix[i][row-1-j] = temp;
             }
         }
     }
