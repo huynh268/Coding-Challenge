6e5785d4949851debd6bfe84098c098f551af8ba
huynh268 <huynhletien@gmail.com>
2017-11-17 22:02:29 -0600

    Added explaination for FindProfession

--- CodeFights/src/FindProfession.java
+++ CodeFights/src/FindProfession.java
@@ -38,7 +38,18 @@ public class FindProfession {
             return pos%2 == 1 ? d : e;
     }
 
-    //Bitwise
+    /*
+    Bitwise
+    Level 1: E
+    Level 2: ED
+    Level 3: EDDE
+    Level 4: EDDEDEED
+    Level 5: EDDEDEEDDEEDEDDE
+
+    Level input is not necessary because first elements are the same
+    The result is based on the count of 1's in binary representation of position-1
+    If position is even, then Engineer; Else Doctor
+    */
     public static String findProfession2(int level, int pos) {
         String  e = "Engineer",
                 d = "Doctor";
