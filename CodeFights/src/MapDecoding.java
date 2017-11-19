eef0c5e86523c53f130d199a2dfbc4c5467ffeb6
huynh268 <huynhletien@gmail.com>
2017-11-18 22:09:36 -0600

    Added MapDecoding

---
+++ CodeFights/src/MapDecoding.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Tien on 11/18/2017.
+ */
+public class MapDecoding {
+    public static int mapDecoding(String message) {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
