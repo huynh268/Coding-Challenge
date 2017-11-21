83d4d6786bda011ff63da938a82af0d033c687fe
huynh268 <huynhletien@gmail.com>
2017-11-20 22:53:51 -0600

    Added FillingBlocks

---
+++ CodeFights/src/FillingBlocks.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Tien on 11/20/2017.
+ * https://codefights.com/interview-practice/task/yWF4MmhvtmzfKNWgt
+ * You have a block with the dimensions 4 × n. Find the number of different ways you can fill this block with smaller blocks that have the dimensions 1 × 2. You are allowed to rotate the smaller blocks.
+
+ Example
+
+ For n = 1, the output should be
+ fillingBlocks(n) = 1.
+
+ There is only one possible way to arrange the smaller 1 × 2 blocks inside the 4 × 1 block.
+
+ For n = 4, the output should be
+ fillingBlocks(n) = 36.
+ */
+public class FillingBlocks {
+}
--- CodeFights/src/MapDecoding.java
+++ CodeFights/src/MapDecoding.java
@@ -20,8 +20,53 @@
  "123" can be decoded as "ABC" (1 2 3), "LC" (12 3) or "AW" (1 23), so the total number of ways is 3.
  */
 public class MapDecoding {
-    public static int mapDecoding(String message) {
-        return 1;
+    public static int mapDecoding1(String s) {
+        int n = s.length();
+
+        if (n <= 0) return 1;
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            int a = Integer.valueOf(s.substring(i-1,i)); //1 digit
+            int b = Integer.valueOf(s.substring(i-2,i)); //2 digits: i and i-1
+
+            //check if 1 digit is valid
+            if (a > 0 && a < 10)
+                dp[i] += dp[i-1]%(1000000007);
+            //check if number with 2 digits is valid (in range [10-26])
+            if (b >= 10 && b <= 26)
+                dp[i] += dp[i-2]%(1000000007);
+        }
+
+        return dp[n]%(1000000007);
+    }
+
+    public static int mapDecoding2(String s) {
+        int n = s.length();
+        if (n <= 0) return 1;
+        if (n == 1 && s.charAt(0) != '0') return 1;
+        int a,b,c;
+        a = b = c = 0;
+        if(s.charAt(0) != '0')
+            a = b = 1;
+
+        for(int i = 1; i < n; i++) {
+            c = 0;
+            if(s.charAt(i) != '0')
+                c += a;
+
+            int d = Integer.valueOf(s.substring(i-1, i+1));
+            if(d >=10 && d <= 26)
+                c += b;
+
+            b = a%(1000000007);
+            a = c%(1000000007);
+        }
+        return c%(1000000007);
     }
 
     public static void main(String[] args) {
