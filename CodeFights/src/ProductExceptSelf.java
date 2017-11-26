0f6f608f51ee2b86ae85c3634fd87dea8a64bcb5
huynh268 <huynhletien@gmail.com>
2017-11-25 23:26:14 -0600

    Update ProductExceptSelf

--- CodeFights/src/ProductExceptSelf.java
+++ CodeFights/src/ProductExceptSelf.java
@@ -3,6 +3,22 @@
  */
 public class ProductExceptSelf {
     public static int productExceptSelf(int[] nums, int m) {
+        Long p = 1L;
 
+        for(int i = 0; i < nums.length; i++){
+            p*=nums[i];
+            p%=m;
+        }
+        System.out.println("p = "+p);
+        Long sum = 0L;
+
+        for(int i = 0; i < nums.length; i++) {
+            sum += (p/nums[i])%m;
+
+        }
+
+
+        //System.out.print(sum);
+        return sum%m;
     }
 }
