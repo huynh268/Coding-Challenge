d8c66ee2b7c4308e2e59aab267984a077b6f9bbb
huynh268 <huynhletien@gmail.com>
2017-12-03 23:47:52 -0600

    Updated MaximalSquare

--- CodeFights/src/MaximalSquare.java
+++ CodeFights/src/MaximalSquare.java
@@ -19,6 +19,26 @@
 public class MaximalSquare {
 
     int maximalSquare(char[][] matrix) {
+        int r = matrix.length;
+        if(r == 0) return 0;
 
+        int c = matrix[0].length;
+        int[][] dp = new int[r][c];
+
+        int max = 0;
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = (int)matrix[i][j]-'0';
+                } else if(matrix[i][j] == '0') {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1])+1;
+                }
+                max = max > dp[i][j] ? max : dp[i][j];
+            }
+        }
+
+        return max*max;
     }
 }
