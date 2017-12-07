784c8fa56972cc814c15370bb9503521586088ae
huynh268 <huynhletien@gmail.com>
2017-12-06 23:09:18 -0600

    Added SwapLexOrder

---
+++ CodeFights/src/HashTables/SwapLexOrder.java
@@ -0,0 +1,51 @@
+package HashTables;
+
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Tien on 12/6/2017.
+ * Given a string str and array of pairs that indicates which indices in the string can be swapped, return the lexicographically largest string that results from doing the allowed swaps. You can swap indices any number of times.
+
+ Example
+
+ For str = "abdc" and pairs = [[1, 4], [3, 4]], the output should be
+ swapLexOrder(str, pairs) = "dbca".
+
+ By swapping the given indices, you get the strings: "cbda", "cbad", "dbac", "dbca". The lexicographically largest string in this list is "dbca".
+ */
+public class SwapLexOrder {
+
+    //Naive Solution - Does not work on big pairs.length
+    String swapLexOrder(String str, int[][] pairs) {
+        if(pairs.length == 0) return str;
+
+        HashSet<String> hs = new HashSet<>();
+        hs.add(str);
+        for(int i = 0; i < pairs.length; i++) {
+            List<String> l = new ArrayList<>(hs);
+
+            for(String s:l) {
+                for(int[] p : pairs)
+                    hs.add(swap(s, p));
+            }
+        }
+
+        List<String> res = new ArrayList<>(hs);
+        Collections.sort(res);
+        return res.get(res.size()-1);
+    }
+
+    String swap(String s, int[] pair) {
+        if(pair[0] == pair[1]) return s;
+        char[] c = s.toCharArray();
+
+        char temp = s.charAt(pair[0]-1);
+        c[pair[0]-1] = c[pair[1]-1];
+        c[pair[1]-1] = temp;
+
+        return new String(c);
+    }
+}
