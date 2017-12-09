e8c6667ccb00e1d3081b54528264612b26639b64
huynh268 <huynhletien@gmail.com>
2017-12-08 20:54:49 -0600

    Added IsListPalindrome

---
+++ CodeFights/src/LinkedLists/IsListPalindrome.java
@@ -0,0 +1,15 @@
+package LinkedLists;
+
+/**
+ * Created by Tien on 12/8/2017.
+ * Given a singly linked list of integers, determine whether or not it's a palindrome.
+
+ Example
+
+ For l = [0, 1, 0], the output should be
+ isListPalindrome(l) = true;
+ For l = [1, 2, 2, 3], the output should be
+ isListPalindrome(l) = false.
+ */
+public class IsListPalindrome {
+}
--- Graph/src/AdjacencyList.java
+++ Graph/src/AdjacencyList.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 
 /**
  * Created by Tien on 12/7/2017.
+ *
+ * Create Adjacency List to represent the graph
  */
 public class AdjacencyList {
 
@@ -20,11 +22,27 @@ public class AdjacencyList {
         }
     }
 
+    /**
+     *
+     * @param graph - graph that has a pair of vertices that will be connected
+     * @param pair - pair of vertices that have the connection
+     */
     static void addEdge(Graph graph, int[] pair) {
-        graph.adjacencyArray[pair[0]].add(pair[1]);
-        graph.adjacencyArray[pair[1]].add(pair[0]);
+        if(pair[0] > pair[1]) {
+            int temp = pair[0];
+            pair[0] = pair[1];
+            pair[1] = temp;
+        }
+        if(!graph.adjacencyArray[pair[0]].contains(pair[1])) {
+            graph.adjacencyArray[pair[0]].add(pair[1]);
+            graph.adjacencyArray[pair[1]].add(pair[0]);
+        }
     }
 
+    /**
+     *
+     * @param graph - graph will be printed out
+     */
     static void printGraph(Graph graph) {
         for(int v = 0; v < graph.V; v++) {
             System.out.print("Adjacency list of vertex "+ v + " : head");
@@ -42,10 +60,11 @@ public class AdjacencyList {
         addEdge(graph,new int[]{0,4});
         addEdge(graph,new int[]{1,2});
         addEdge(graph,new int[]{1,3});
+        addEdge(graph,new int[]{4,1});
         addEdge(graph,new int[]{2,3});
         addEdge(graph,new int[]{2,4});
         addEdge(graph,new int[]{3,4});
-
+        addEdge(graph,new int[]{3,4});
         printGraph(graph);
     }
 }
