642d420ceda65af7c4fd1eae0bbf966a3cd622dc
huynh268 <huynhletien@gmail.com>
2017-12-13 18:50:01 -0600

    Updated SimplifyPath

--- CodeFights/src/HeapsStacksQueues/SimplifyPath.java
+++ CodeFights/src/HeapsStacksQueues/SimplifyPath.java
@@ -1,7 +1,50 @@
 package HeapsStacksQueues;
 
+import java.util.Stack;
+
 /**
  * Created by Tien on 12/12/2017.
+ *
+ * Given an absolute file path (Unix-style), shorten it to the format /<dir1>/<dir2>/<dir3>/....
+
+ Here is some info on Unix file system paths:
+
+ / is the root directory; the path should always start with it even if it isn't there in the given path;
+ / is also used as a directory separator; for example, /code/fights denotes a fights subfolder in the code folder in the root directory;
+ this also means that // stands for "change the current directory to the current directory"
+ . is used to mark the current directory;
+ .. is used to mark the parent directory; if the current directory is root already, .. does nothing.
+ Example
+
+ For path = "/home/a/./x/../b//c/", the output should be
+ simplifyPath(path) = "/home/a/b/c".
+
+ Here is how this path was simplified:
+ * /./ means "move to the current directory" and can be replaced with a single /;
+ * /x/../ means "move into directory x and then return back to the parent directory", so it can replaced with a single /;
+ * // means "move to the current directory" and can be replaced with a single /.
  */
 public class SimplifyPath {
+
+    String simplifyPath(String path) {
+        String[] str = path.split("/");
+
+        Stack<String> stack = new Stack<>();
+
+        for(String s: str) {
+            if(s.equals("") || s.equals(".") || s.equals("..") && stack.isEmpty())
+                continue;
+            else if(s.equals(".."))
+                stack.pop();
+            else
+                stack.push(s);
+        }
+
+        String result = "";
+
+        while(!stack.isEmpty()) {
+            result = "/" + stack.pop()+result ;
+        }
+        return result.length() > 0 ? result : "/";
+    }
 }
