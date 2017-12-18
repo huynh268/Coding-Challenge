3ec1e65375e77174fbbd78d45fc6a104a0df76d1
huynh268 <huynhletien@gmail.com>
2017-12-17 19:20:18 -0600

    Updated IsSubsTree

--- CodeFights/src/Trees/IsSubtree.java
+++ CodeFights/src/Trees/IsSubtree.java
@@ -1,5 +1,10 @@
 package Trees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * Created by Tien on 12/10/2017.
  */
@@ -14,4 +19,22 @@ public class IsSubtree {
     //   Tree<T> left;
     //   Tree<T> right;
     // }
+
+    int[] traverseTree(Tree<Integer> t) {
+        if(t == null) return new int[0];
+
+        List<Integer> list = new ArrayList<>();
+        Queue<Tree<Integer>> queue = new LinkedList<>();
+        queue.add(t);
+
+        while(!queue.isEmpty()) {
+            t = queue.poll();
+            list.add(t.value);
+            if(t.left !=  null) queue.add(t.left);
+            if(t.right != null) queue.add(t.right);
+        }
+
+        return list.stream().mapToInt(i->i).toArray();
+    }
+
 }
