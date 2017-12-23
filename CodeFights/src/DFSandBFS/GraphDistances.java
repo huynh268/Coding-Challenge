747a0d70d2cabd41dca3aeebd14b9fd3ee1cc40b
huynh268 <huynhletien@gmail.com>
2017-12-23 01:14:35 -0600

    Updated GraphDistances

--- CodeFights/src/DFSandBFS/GraphDistances.java
+++ CodeFights/src/DFSandBFS/GraphDistances.java
@@ -2,6 +2,26 @@ package DFSandBFS;
 
 /**
  * Created by Tien on 12/18/2017.
+ *
+ * You have a connected directed graph that has positive weights in the adjacency matrix g.
+ * The graph is represented as a square matrix, where g[i][j] is the weight of the edge (i, j), or -1 if there is no such edge.
+
+ Given g and the index of a start vertex s, find the minimal distances between the start vertex s and each of the vertices of the graph.
+
+ Example
+
+ For
+
+ g = [[-1, 3, 2],
+ [2, -1, 0],
+ [-1, 0, -1]]
+ and s = 0, the output should be
+ graphDistances(g, s) = [0, 2, 2].
  */
 public class GraphDistances {
+
+    int[] graphDistances(int[][] g, int s) {
+        return new int[0];
+    }
+
 }
