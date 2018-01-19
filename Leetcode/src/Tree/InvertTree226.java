ddcb33b2cff6a99756863168bfa0e15a295d441e
huynh268 <huynhletien@gmail.com>
2018-01-18 23:26:17 -0600

    Updated InvertTree226

--- Leetcode/src/Tree/InvertTree226.java
+++ Leetcode/src/Tree/InvertTree226.java
@@ -1,7 +1,13 @@
 package Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Created by Tien on 1/2/2018.
+ *
+ * https://leetcode.com/problems/invert-binary-tree/description/
  *
  * Invert a binary tree.
       4
@@ -19,15 +25,81 @@ package Tree;
  9   6 3   1
  */
 public class InvertTree226 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
 
-    TreeNode invertTree(TreeNode root) {
+    /**
+     * Recursive
+     * O(n) Time complexity
+     * O(n) Space
+     * @param root
+     * @return
+     */
+    TreeNode invertTree1(TreeNode root) {
         if(root != null) {
             TreeNode temp = root.left;
             root.left = root.right;
             root.right = temp;
-            if(root.left != null) invertTree(root.left);
-            if(root.right != null) invertTree(root.right);
+            if(root.left != null) invertTree1(root.left);
+            if(root.right != null) invertTree1(root.right);
         }
+        return root;
+    }
+
+    /**
+     * Iterative - Queue
+     * O(n) Time complexity
+     * O(n) Space
+     * @param root
+     * @return
+     */
+    TreeNode invertTree2(TreeNode root) {
+        if(root == null) return root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            TreeNode currentNode = queue.poll();
+            TreeNode tmp = currentNode.left;
+            currentNode.left = currentNode.right;
+            currentNode.right = tmp;
+
+            if(currentNode.left != null) queue.add(currentNode.left);
+            if(currentNode.right != null) queue.add(currentNode.right);
+        }
+
+        return root;
+    }
+
+    /**
+     *  Iterative - Stack
+     * O(n) Time complexity
+     * O(n) Space
+     * @param root
+     * @return
+     */
+    TreeNode invertTree3(TreeNode root) {
+        if(root == null) return root;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()) {
+            TreeNode currentNode = stack.pop();
+            TreeNode tmp = currentNode.left;
+            currentNode.left = currentNode.right;
+            currentNode.right = tmp;
+
+            if(currentNode.left != null) stack.push(currentNode.left);
+            if(currentNode.right != null) stack.push(currentNode.right);
+        }
+
         return root;
     }
 }
