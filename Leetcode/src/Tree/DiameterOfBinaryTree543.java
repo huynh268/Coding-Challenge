4dac94941780d162147d93da5599d88b9b14cd07
huynh268 <huynhletien@gmail.com>
2018-01-21 23:35:45 -0600

    Updated DiameterOfBinaryTree543

--- Leetcode/src/Tree/DiameterOfBinaryTree543.java
+++ Leetcode/src/Tree/DiameterOfBinaryTree543.java
@@ -30,7 +30,51 @@ public class DiameterOfBinaryTree543 {
      * }
      */
 
-    int diameterOfBinaryTree(TreeNode root) {
-        
+    /**
+     * O(nlogn) Time complexity
+     * O(n) Space
+     *
+     * Credit: daydayup for the explanation of time complexity
+     * diameterOfBinaryTree is called on every node. In each call, it traverses all descendants of that node to get the depth.
+
+        for root node, it is n => n + 1 - 2^0
+        for all nodes on 2nd level, it is 2 * (n - 1) / 2 => n - 1 => n + 1 - 2^1
+        for all nodes on 3rd level, it is 4 * (n - 3) / 4 => n - 3 => n + 1 - 2^2
+        for all nodes on 4th level, it is 8 * (n - 7) / 8 => n - 7 => n + 1 - 2^3
+        ...
+        for all nodes on last level, it is n + 1 - 2^(h-1). h is max tree depth.
+        Add them up, the result is (n+1) * h - (1 + 2 + 4 ... + 2^(h-1)). In worst case, the latter part is n (all nodes in the tree), so time complexity is O(n*logn).
+     * @param root
+     * @return
+     */
+    int diameterOfBinaryTree1(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(maxDepth1(root.left) + maxDepth1(root.right),
+                Math.max(diameterOfBinaryTree1(root.left), diameterOfBinaryTree1(root.right)));
+    }
+
+    int maxDepth1(TreeNode root) {
+        if(root == null) return 0;
+        return 1 + Math.max(maxDepth1(root.left), maxDepth1(root.right));
+    }
+
+
+    /**
+     * O(n) Time complexity - Each node is visited once
+     * O(n) Space
+     */
+    int max = 0;
+    int diameterOfBinaryTree2(TreeNode root) {
+        if(root == null) return 0;
+        maxDepth2(root);
+        return max;
+    }
+
+    int maxDepth2(TreeNode root) {
+        if(root == null) return 0;
+        int leftDepth = maxDepth2(root.left);
+        int rightDepth = maxDepth2(root.right);
+        max = Math.max(max, leftDepth + rightDepth);
+        return 1 + Math.max(leftDepth, rightDepth);
     }
 }
