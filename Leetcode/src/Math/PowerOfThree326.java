42e8edddf9e6f7d0f40c0fce8ab8df149e5ddacb
huynh268 <huynhletien@gmail.com>
2018-02-18 23:50:56 -0600

    Added PowerOfThree326

---
+++ Leetcode/src/Math/PowerOfThree326.java
@@ -0,0 +1,36 @@
+package Math;
+
+/**
+ * Created by Tien on 2/18/2018.
+ *
+ * https://leetcode.com/problems/power-of-three/description/
+ *
+ * Given an integer, write a function to determine if it is a power of three.
+
+ Follow up:
+ Could you do it without using any loop / recursion?
+ */
+public class PowerOfThree326 {
+
+    /**
+     * O(logn) Time complexity
+     * O(1) Space
+     * @param n
+     * @return
+     */
+    boolean isPowerOfThree1(int n) {
+        if(n == 0) return false;
+        while(n%3 == 0)
+            n /= 3;
+        return n == 1;
+    }
+
+    /**
+     * LOGa(b) = LOGc(b) / LOGc(a)
+     * @param n
+     * @return
+     */
+    boolean isPowerOfThree2(int n) {
+        return (Math.log10(n) / Math.log10(3)) % 1 == 0?
+    }
+}
