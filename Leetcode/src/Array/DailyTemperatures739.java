c47da000e1da21cc0d6357542bf6d9da34388516
huynh268 <huynhletien@gmail.com>
2018-02-22 23:49:16 -0600

    Added DailyTemperatures739

---
+++ Leetcode/src/Array/DailyTemperatures739.java
@@ -0,0 +1,40 @@
+package Array;
+
+/**
+ * Created by Tien on 2/22/2018.
+ *
+ * https://leetcode.com/problems/daily-temperatures/description/
+ *
+ * Given a list of daily temperatures, produce a list that, for each day in the input, tells you how many days you would have to wait until a warmer temperature.
+ * If there is no future day for which this is possible, put 0 instead.
+
+ For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+ Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+ */
+public class DailyTemperatures739 {
+
+    /**
+     * Brute force
+     * O(n^2) Time complexity
+     * O(n) Space
+     * @param temperatures
+     * @return
+     */
+    int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        for(int i = 0; i < n; i++) {
+            if(i == n-1) ans[i] = 0;
+            for(int j = i+1; j < n; j++) {
+                if(j == n-1 && temperatures[i] >= temperatures[j]) ans[i] = 0;
+                if(temperatures[i] >= temperatures[j]) continue;
+                else {
+                    ans[i] = j - i;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
