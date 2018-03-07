4c8c267a93abc16c6e9f9dc2f604e13adda290e0
huynh268 <huynhletien@gmail.com>
2018-03-06 23:43:53 -0600

    Added NextGreaterElementI496

---
+++ Leetcode/src/Array/NextGreaterElementI496.java
@@ -0,0 +1,67 @@
+package Array;
+
+import java.util.HashMap;
+
+/**
+ * Created by Tien on 3/6/2018.
+ *
+ * https://leetcode.com/problems/next-greater-element-i/description/
+ *
+ * You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2.
+ * Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+
+ The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+
+ Example 1:
+ Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ Output: [-1,3,-1]
+ Explanation:
+ For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+ For number 1 in the first array, the next greater number for it in the second array is 3.
+ For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+
+ Example 2:
+ Input: nums1 = [2,4], nums2 = [1,2,3,4].
+ Output: [3,-1]
+ Explanation:
+ For number 2 in the first array, the next greater number for it in the second array is 3.
+ For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+
+ Note:
+ All elements in nums1 and nums2 are unique.
+ The length of both nums1 and nums2 would not exceed 1000.
+
+ */
+public class NextGreaterElementI496 {
+
+    /**
+     * O(n*m) Time complexity
+     * O(n+m) Space
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for(int i = 0; i < nums2.length; i++) {
+            hashMap.put(nums2[i], i);
+        }
+
+        for(int i = 0; i < nums1.length; i++) {
+            int j = hashMap.get(nums1[i]) + 1;
+            while(j <= nums2.length) {
+                if(j == nums2.length) {
+                    ans[i] = -1;
+                    break;
+                }else if(nums2[j] > nums1[i]) {
+                    ans[i] = nums2[j];
+                    break;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return ans;
+    }
+}
