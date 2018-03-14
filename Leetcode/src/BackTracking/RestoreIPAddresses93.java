62909d1d254c34cac8e759d8bc51b91bc6ea381d
huynh268 <huynhletien@gmail.com>
2018-03-13 23:09:48 -0500

    Added RestoreIPAddresses93

---
+++ Leetcode/src/BackTracking/RestoreIPAddresses93.java
@@ -0,0 +1,23 @@
+package BackTracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Tien on 3/13/2018.
+ *
+ * https://leetcode.com/problems/restore-ip-addresses/description/
+ *
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+ For example:
+ Given "25525511135",
+
+ return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+ */
+public class RestoreIPAddresses93 {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>();
+        return ans;
+    }
+}
