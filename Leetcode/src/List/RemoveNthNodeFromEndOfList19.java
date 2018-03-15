df56e890c4bed4b318eaf48310f66ed7f7a8eae5
huynh268 <huynhletien@gmail.com>
2018-03-14 22:29:42 -0500

    Added solutions for RemoveNthNodeFromEndOfList19

--- Leetcode/src/List/RemoveNthNodeFromEndOfList19.java
+++ Leetcode/src/List/RemoveNthNodeFromEndOfList19.java
@@ -25,7 +25,66 @@ public class RemoveNthNodeFromEndOfList19 {
      *     ListNode(int x) { val = x; }
      * }
      */
-    public ListNode removeNthFromEnd(ListNode head, int n) {
 
+    /**
+     * Two pass - First pass to count the length of the list
+     *          - Second pass to remove the nth node from the end of the list
+     * O(N) Time complexity
+     * O(1) Space
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode currentNode = head;
+        int count = 0;
+
+        while(currentNode != null) {
+            currentNode = currentNode.next;
+            count++;
+        }
+
+        currentNode = dummy;
+        count -= n;
+        while(count > 0) {
+            currentNode = currentNode.next;
+            count--;
+        }
+
+        currentNode.next = currentNode.next.next;
+
+        return head;
+    }
+
+    /**
+     * One pass - Maintain the distance between first and second nodes equal to n
+     *          - When first reaches the end of the list, second reaches the (n+1)th node from the end of the list
+     * O(N) Time complexity
+     * O(1) Space
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode first = head;
+        ListNode second = new ListNode(-1);
+        ListNode dummy = second;
+        second.next = head;
+
+        while(n > 0) {
+            first = first.next;
+            n--;
+        }
+
+        while(first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+
+        return dummy.next;
     }
 }
