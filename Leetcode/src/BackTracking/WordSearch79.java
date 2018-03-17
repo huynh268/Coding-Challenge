3f4f4c68f159aad8dddb2e6db6cb72107d71a6c1
huynh268 <huynhletien@gmail.com>
2018-03-16 22:29:45 -0500

    Added WordSearch79

--- Leetcode/src/BackTracking/WordSearch79.java
+++ Leetcode/src/BackTracking/WordSearch79.java
@@ -24,4 +24,37 @@ package BackTracking;
 
  */
 public class WordSearch79 {
+
+    /**
+     * Backtracking
+     * O(n*m*4^k) Time complexity - where n, m are board row and col lengths.
+     *                            - there are 4 recursive calls for each step - 4^k
+     * O(1) Space - Since there is no extra space used
+     * @param board
+     * @param word
+     * @return
+     */
+    public boolean exist(char[][] board, String word) {
+        char[] chars = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(check(board, chars, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(char[][] board, char[] word, int row, int col, int i) {
+        if(i == word.length) return true;
+        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length) return false;
+        if(board[row][col] != word[i]) return false;
+
+        board[row][col] = '*';
+        boolean exist = check(board, word, row-1, col, i+1) ||
+                        check(board, word, row+1, col, i+1) ||
+                        check(board, word, row, col-1, i+1) ||
+                        check(board, word, row, col+1, i+1);
+        board[row][col] = word[i];
+        return exist;
+    }
 }
