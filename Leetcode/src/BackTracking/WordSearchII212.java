609aa67a7921da7e8cf1d2fc1ba1b422ba336bf0
huynh268 <huynhletien@gmail.com>
2018-03-16 22:31:16 -0500

    Added WordSearchII212

---
+++ Leetcode/src/BackTracking/WordSearchII212.java
@@ -0,0 +1,35 @@
+package BackTracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Tien on 3/16/2018.
+ *
+ * https://leetcode.com/problems/word-search-ii/description/
+ *
+ * Given a 2D board and a list of words from the dictionary, find all words in the board.
+
+ Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring.
+ The same letter cell may not be used more than once in a word.
+
+ For example,
+ Given words = ["oath","pea","eat","rain"] and board =
+
+ [
+ ['o','a','a','n'],
+ ['e','t','a','e'],
+ ['i','h','k','r'],
+ ['i','f','l','v']
+ ]
+ Return ["eat","oath"].
+ Note:
+ You may assume that all inputs are consist of lowercase letters a-z.
+ */
+public class WordSearchII212 {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> ans = new ArrayList<>();
+
+        return ans;
+    }
+}
