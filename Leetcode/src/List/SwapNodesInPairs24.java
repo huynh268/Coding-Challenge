52527fc7dfb592c3e597e5ae30878d0273411349
huynh268 <huynhletien@gmail.com>
2018-03-22 22:33:32 -0500

    Added SwapNodesInPairs24

---
+++ Leetcode/src/List/SwapNodesInPairs24.java
@@ -0,0 +1,28 @@
+package List;
+
+/**
+ * Created by Tien on 3/22/2018
+ *
+ * https://leetcode.com/problems/swap-nodes-in-pairs/description/
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head.
+
+ For example,
+ Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+ Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+ */
+public class SwapNodesInPairs24 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+
+    public ListNode swapPairs(ListNode head) {
+
+    }
+}
