fc71eaa72ae3d41d9d9f38c10d5591f5d1ded478
huynh268 <huynhletien@gmail.com>
2018-03-23 22:10:25 -0500

    Added InsertionSortList147

---
+++ Leetcode/src/List/InsertionSortList147.java
@@ -0,0 +1,23 @@
+package List;
+
+/**
+ * Created by Tien on 3/23/2018.
+ *
+ * https://leetcode.com/problems/insertion-sort-list/description/
+ *
+ * Sort a linked list using insertion sort.
+ */
+public class InsertionSortList147 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+
+    public ListNode insertionSortList(ListNode head) {
+
+    }
+}
