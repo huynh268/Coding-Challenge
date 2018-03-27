761bbaac3281a2a189ec2093f2048991550b9110
huynh268 <huynhletien@gmail.com>
2018-03-26 22:38:55 -0500

    Added SortColors75

--- Leetcode/src/Array/SortColors75.java
+++ Leetcode/src/Array/SortColors75.java
@@ -14,7 +14,26 @@ package Array;
 
  */
 public class SortColors75 {
-    public void sortColors(int[] nums) {
+
+    /**
+     * Buble sort
+     * O(N^2) Time complexity
+     * O(1) Space
+     * @param nums
+     */
+    public void sortColors1(int[] nums) {
+        for(int i = 0; i < nums.length-1; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                if(nums[i] > nums[j]) {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                }
+            }
+        }
+    }
+
+    public void sortColors2(int[] nums) {
 
     }
 }
