c65b0dddf4bd89bc18edac335e32894aafe7ef37
huynh268 <huynhletien@gmail.com>
2018-03-30 22:32:53 -0500

    Added LongestCommonPrefix14

---
+++ Leetcode/src/String/LongestCommonPrefix14.java
@@ -0,0 +1,14 @@
+package String;
+
+/**
+ * Created by Tien on 3/30/2018.
+ *
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ *
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ */
+public class LongestCommonPrefix14 {
+    public String longestCommonPrefix(String[] strs) {
+
+    }
+}
