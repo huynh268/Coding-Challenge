ce80afcab9a90ae29049b9de3cfcd869e24f1154
huynh268 <huynhletien@gmail.com>
2018-04-17 23:19:54 -0700

    Added SubarraySumEqualsK560

---
+++ Leetcode/src/Array/SubarraySumEqualsK560.java
@@ -0,0 +1,22 @@
+package Array;
+
+/**
+ * Created by Tien on 4/17/2018.
+ *
+ * https://leetcode.com/problems/subarray-sum-equals-k/description/
+ *
+ * Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+ Example 1:
+ Input:nums = [1,1,1], k = 2
+ Output: 2
+
+ Note:
+ The length of the array is in range [1, 20,000].
+ The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+ */
+public class SubarraySumEqualsK560 {
+    public int subarraySum(int[] nums, int k) {
+
+    }
+}
