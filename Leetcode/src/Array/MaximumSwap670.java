bcb776e75a30b8b7888051c13d776a69e62fd1e3
huynh268 <huynhletien@gmail.com>
2018-04-18 22:14:37 -0700

    Added MaximumSwap670

---
+++ Leetcode/src/Array/MaximumSwap670.java
@@ -0,0 +1,28 @@
+package Array;
+
+/**
+ * Created by Tien on 4/18/2018.
+ *
+ * https://leetcode.com/problems/maximum-swap/description/
+ *
+ * Given a non-negative integer, you could swap two digits at most once to get the maximum valued number. Return the maximum valued number you could get.
+
+ Example 1:
+ Input: 2736
+ Output: 7236
+ Explanation: Swap the number 2 and the number 7.
+
+ Example 2:
+ Input: 9973
+ Output: 9973
+ Explanation: No swap.
+
+ Note:
+ The given number is in the range [0, 10^8]
+ */
+public class MaximumSwap670 {
+
+    public int maximumSwap(int num) {
+
+    }
+}
