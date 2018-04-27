94714156ca9898c7cf4bc13bd163dad371e2dd85
huynh268 <huynhletien@gmail.com>
2018-04-26 23:47:07 -0700

    Added solution for FriendCircles547

--- Leetcode/src/DFSandBFS/FriendCircles547.java
+++ Leetcode/src/DFSandBFS/FriendCircles547.java
@@ -36,7 +36,31 @@ package DFSandBFS;
  */
 public class FriendCircles547 {
 
+    /**
+     * DFS
+     * O(n^2) Time complexity
+     * O(1) Space
+     * @param M
+     * @return
+     */
     public int findCircleNum(int[][] M) {
+        int ans = 0;
+        for(int i = 0; i < M.length; i++) {
+            if(M[i][i] == 1) {
+                ans++;
+                dfs(M, i);
+                M[i][i] = 0;
+            }
+        }
+        return ans;
+    }
 
+    private void dfs(int[][] M, int i) {
+        for(int j = 0; j < M.length; j++) {
+            if(M[j][j] == 1 && M[i][j] == 1) {
+                M[j][j] = 0;
+                dfs(M, j);
+            }
+        }
     }
 }
