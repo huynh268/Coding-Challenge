31b38f26dcf1b44be097d3921c4dd72348e9226c
huynh268 <huynhletien@gmail.com>
2018-05-01 22:48:46 -0700

    Added solutions for SubarraySumEqualsK560

--- Leetcode/src/Array/SubarraySumEqualsK560.java
+++ Leetcode/src/Array/SubarraySumEqualsK560.java
@@ -1,5 +1,7 @@
 package Array;
 
+import java.util.HashMap;
+
 /**
  * Created by Tien on 4/17/2018.
  *
@@ -16,7 +18,80 @@ package Array;
  The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
  */
 public class SubarraySumEqualsK560 {
-    public int subarraySum(int[] nums, int k) {
 
+    /**
+     * Prefix Sum
+     * O(n^2) Time complexity
+     * O(n) Space
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum1(int[] nums, int k) {
+        int[] prefixSum = new int[nums.length + 1];
+        prefixSum[0] = 0;
+        for(int i = 1; i < prefixSum.length; i++)
+            prefixSum[i] = prefixSum[i-1] + nums[i-1];
+
+        int count = 0;
+        for(int i = 1; i < prefixSum.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(prefixSum[i] - prefixSum[j] == k) count++;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Prefix Sum + Hashtable
+     * O(n) Time complexity
+     * O(n) Space
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum2(int[] nums, int k) {
+        int[] prefixSum = new int[nums.length + 1];
+        prefixSum[0] = 0;
+        for(int i = 1; i < prefixSum.length; i++)
+            prefixSum[i] = prefixSum[i-1] + nums[i-1];
+
+        int count = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for(int i = 0; i < prefixSum.length; i++) {
+            if(hashMap.containsKey(prefixSum[i] - k)) {
+                count += hashMap.get(prefixSum[i] - k);
+            }
+            hashMap.put(prefixSum[i], hashMap.getOrDefault(prefixSum[i], 0) + 1);
+        }
+
+        return count;
+    }
+
+    /**
+     * Prefix Sum(without storing array) + Hashtable
+     * O(n) Time complexity
+     * O(n) Space
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum3(int[] nums, int k) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        int sum = 0;
+        int count = 0;
+
+        hashMap.put(0, 1); //nums[i] = k
+
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(hashMap.containsKey(sum - k)) {
+                count += hashMap.get(sum - k);
+            }
+            hashMap.put(sum, hashMap.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
     }
 }
