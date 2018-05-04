94506d584987aa16410a967dcc3a3c827c279666
huynh268 <huynhletien@gmail.com>
2018-05-03 22:41:52 -0700

    Added ValidSudoku36

--- Leetcode/src/Array/ValidSudoku36.java
+++ Leetcode/src/Array/ValidSudoku36.java
@@ -1,9 +1,10 @@
 package Array;
 
+import java.util.HashSet;
 /**
  * Created by Tien on 5/2/2018.
  *
- *
+ * https://leetcode.com/problems/valid-sudoku/description/https://leetcode.com/problems/valid-sudoku/description/
  *
  * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
  -Each row must contain the digits 1-9 without repetition.
@@ -51,7 +52,54 @@ package Array;
  The given board size is always 9x9.
  */
 public class ValidSudoku36 {
-    public boolean isValidSudoku(char[][] board) {
 
+    /**
+     * O(n^2) Time complexity
+     * O(n^2) Space
+     * @param board
+     * @return
+     */
+    public boolean isValidSudoku1(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] column = new boolean[9][9];
+        boolean[][] block = new boolean[9][9];
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                int n = board[i][j] - '1';
+                if(board[i][j] != '.') {
+                    if(row[i][n] || column[j][n] || block[i - i%3 +j/3][n]) return false;
+                    row[i][n] = true;
+                    column[j][n] = true;
+                    block[i - i%3 + j/3][n] = true;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+    /**
+     * O(n^2) Time complexity
+     * O(n^2) Space
+     * @param board
+     * @return
+     */
+    public boolean isValidSudoku2(char[][] board) {
+        HashSet<String> hashSet = new HashSet<>();
+
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] != '.') {
+                    String checkRow = "row: " + i + board[i][j];
+                    String checkCol = "col: " + j + board[i][j];
+                    String checkBlock = "block: " + (i - i%3 + j/3) + board[i][j];
+                    if(!hashSet.add(checkRow) || !hashSet.add(checkCol) || !hashSet.add(checkBlock)) return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
