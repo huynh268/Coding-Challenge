4da26decad45184ef3df3535abe45b868242a647
huynh268 <huynhletien@gmail.com>
2018-05-10 21:57:07 -0700

    Added solution for EvaluateReversePolishNotation150

--- Leetcode/src/HeapStackQueue/EvaluateReversePolishNotation150.java
+++ Leetcode/src/HeapStackQueue/EvaluateReversePolishNotation150.java
@@ -35,6 +35,24 @@ package HeapStackQueue;
 public class EvaluateReversePolishNotation150 {
 
     public int evalRPN(String[] tokens) {
+        Stack<Integer> s = new Stack<>();
 
+        for(int i = 0; i < tokens.length; i++) {
+            if(tokens[i].equals("+")) {
+                s.push(s.pop() + s.pop());
+            } else if (tokens[i].equals("-")) {
+                s.push(-s.pop() + s.pop());
+            } else if (tokens[i].equals("*")) {
+                s.push(s.pop() * s.pop());
+            } else if (tokens[i].equals("/")) {
+                int a = s.pop();
+                int b = s.pop();
+                s.push(b / a);
+            } else {
+                s.push(Integer.valueOf(tokens[i]));
+            }
+        }
+
+        return s.pop();
     }
 }
