e3f92a295dfd773ace42b4c2f66432f6a80a9a72
huynh268 <huynhletien@gmail.com>
2018-05-15 23:32:44 -0700

    Added LongestHarmoniousSubsequence594

---
+++ Leetcode/src/Array/LongestHarmoniousSubsequence594.java
@@ -0,0 +1,25 @@
+package Array;
+
+/**
+ * Created by Tien on 5/15/2018.
+ *
+ * https://leetcode.com/problems/longest-harmonious-subsequence/description/
+ *
+ * We define a harmonious array is an array where the difference between its maximum value and its minimum value is exactly 1.
+ Now, given an integer array, you need to find the length of its longest harmonious subsequence among all its possible subsequences.
+
+ Example 1:
+ Input: [1,3,2,2,5,2,3,7]
+ Output: 5
+ Explanation: The longest harmonious subsequence is [3,2,2,2,3].
+
+ Note: The length of the input array will not exceed 20,000.
+
+
+ */
+public class LongestHarmoniousSubsequence594 {
+
+    public int findLHS(int[] nums) {
+
+    }
+}
