079f3eea5ea9e16b2ff5689c814047473ce4e5da
huynh268 <huynhletien@gmail.com>
2018-05-25 22:38:31 -0700

    Added solution for ReverseBits190

--- Leetcode/src/BitManipulation/ReverseBits190.java
+++ Leetcode/src/BitManipulation/ReverseBits190.java
@@ -14,8 +14,15 @@ package BitManipulation;
  If this function is called many times, how would you optimize it?
  */
 public class ReverseBits190 {
+
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans += n&1;
+            n >>= 1;
+        }
+        return ans;
     }
 }
