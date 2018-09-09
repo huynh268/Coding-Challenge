bf2801755b351d50f3974823171f943a89565326
huynh268 <huynhletien@gmail.com>
2018-09-08 23:41:40 -0700

    add WordSearchII212

--- Leetcode/src/BackTracking/WordSearchII212.java
+++ Leetcode/src/BackTracking/WordSearchII212.java
@@ -1,5 +1,6 @@
 package BackTracking;
 
+import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,13 +28,105 @@ import java.util.List;
  You may assume that all inputs are consist of lowercase letters a-z.
  */
 public class WordSearchII212 {
-    public List<String> findWords(char[][] board, String[] words) {
-        for(String word : words) {
-            trie.
+
+    public class TrieNode {
+        TrieNode[] nodes;
+        String word = "";
+
+        public TrieNode() {
+            this.nodes = new TrieNode[26];
         }
 
-        List<String> ans = new ArrayList<>();
+        public boolean containsChar(char c) {
+            return nodes[c - 'a'] != null;
+        }
 
-        return ans;
+        public void put(char c, TrieNode node) {
+            nodes[c - 'a'] = node;
+        }
+
+        public TrieNode get(char c) {
+            return nodes[c - 'a'];
+        }
+
+        public void setWord(String word) {
+            this.word = word;
+        }
+    }
+
+    public class Trie {
+        TrieNode root;
+        public Trie() {
+            this.root = new TrieNode();
+        }
+
+        public void insert(String[] words) {
+            for(String word : words) {
+                insert(word);
+            }
+        }
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for(char c : word.toCharArray()) {
+                if(!node.containsChar(c)) {
+                    node.put(c, new TrieNode());
+                }
+                node = node.get(c);
+            }
+            node.setWord(word);
+        }
+
+        public boolean search(String word) {
+            TrieNode node = root;
+            for(char c : word.toCharArray()) {
+                if(!node.containsChar(c)) return false;
+                node = node.get(c);
+            }
+            return node.word.equals(word);
+        }
+
+        public boolean startsWith(String prefix) {
+            TrieNode node = root;
+            for(char c : prefix.toCharArray()) {
+                if(!node.containsChar(c)) return false;
+                node = node.get(c);
+            }
+            return true;
+        }
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+        HashSet<String> ans = new HashSet<>();
+        Trie trie = new Trie();
+        trie.insert(words);
+
+        boolean[][] visited = new boolean[board.length][board[0].length];
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                dfs(board, "", trie, ans, i, j, visited);
+            }
+        }
+
+        return new ArrayList<String>(ans);
+    }
+
+    public void dfs(char[][] board, String s, Trie trie, HashSet<String> ans, int i, int j, boolean[][] visited) {
+        if(i < 0 || j < 0 || i == board.length || j == board[0].length || visited[i][j]) {
+            return;
+        } else {
+            s += board[i][j];
+
+            if(!trie.startsWith(s)) return;
+            if(trie.search(s)) ans.add(s);
+
+            visited[i][j] = true;
+            dfs(board, s, trie, ans, i-1, j, visited);
+            dfs(board, s, trie, ans, i+1, j, visited);
+            dfs(board, s, trie, ans, i, j-1, visited);
+            dfs(board, s, trie, ans, i, j+1, visited);
+            visited[i][j] = false;
+        }
     }
 }
